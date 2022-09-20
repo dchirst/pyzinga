9194539260f4075b98c48bef92ea0e1df7e7bfa5
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-09-20 21:25:14 +0100

    Added initialization for Board

--- src/pyzinga/board.py
+++ src/pyzinga/board.py
@@ -1,4 +1,20 @@
+from game_type import GameType
+from pyzinga.game_string import GameString
 
 
 class Board:
-    pass
+    def __init__(self, game_string: GameString | str = None):
+        self.game_string = game_string if isinstance(game_string, GameString) else GameString(game_string)
+
+    @classmethod
+    def from_game_type_string(cls, game_type_string):
+        return Board(game_string=GameString(type=game_type_string))
+
+    @classmethod
+    def from_game_string(cls, game_string):
+        return Board(game_string=game_string)
+
+    def __str__(self):
+        return self.game_string
+
+
