05e3d2381017e68f95186c08bf51cd95a109e119
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-09-20 21:53:13 +0100

    Added GameState object

--- src/pyzinga/enums.py
+++ src/pyzinga/enums.py
@@ -4,3 +4,12 @@ from enum import Enum
 class Color(Enum):
     White = 1
     Black = 2
+
+
+class GameStateEnum(Enum):
+    NotStarted = 1
+    InProgress = 2
+    Draw = 3
+    WhiteWins = 4
+    BlackWins = 5
+
--- src/pyzinga/game_state.py
+++ src/pyzinga/game_state.py
@@ -1,4 +1,19 @@
+from dataclasses import dataclass
+
+from pyzinga.enums import GameStateEnum
 
 
+@dataclass
 class GameState:
-    pass
+    state: GameStateEnum = GameStateEnum.NotStarted
+
+    @classmethod
+    def from_string(cls, state_string: str):
+        return GameState(state=GameStateEnum[state_string])
+
+    @classmethod
+    def is_valid(cls, state_string: str):
+        return state_string in [e.name for e in GameStateEnum]
+
+    def __str__(self):
+        return self.state.name
