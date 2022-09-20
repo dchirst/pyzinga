c5952ab19c153f54e3ca7f7b130f38d206dd8118
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-09-20 21:43:17 +0100

    Added GameTurn object

--- src/pyzinga/game_turn.py
+++ src/pyzinga/game_turn.py
@@ -1,4 +1,20 @@
+from dataclasses import dataclass
+from enums import Color
 
 
 class GameTurn:
-    pass
+    color: Color = Color.White
+    turn: int = 1
+
+    def __str__(self):
+        return f"{self.color.name}[{self.turn}]"
+
+    def increment(self):
+        if self.color == Color.White:
+            self.color = Color.Black
+        elif self.color == Color.Black:
+            self.color = Color.White
+            self.turn += 1
+        else:
+            raise Exception("GameType attribute 'color' has a value of neither WHITE nor BLACK. Something is going wrong"
+                            "here.")
